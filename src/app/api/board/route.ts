131e8959177575201668f0e9a016be159abbc69b
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-07-10 00:09:41 +0900

    Trelloクローンアプリ作成(PR指摘対応5)

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -1,20 +1,29 @@
 import { NextResponse } from 'next/server';
-import path from 'path';
-import fs from 'fs/promises';
 import { Board } from '@/types';
 import { boardSchema } from '@/validation/boardValidation';
 import z from 'zod';
 import { auth } from '@/auth';
+import { kv } from '@vercel/kv';
+
+const BOARD_KEY = 'board';
 
 export async function GET() {
   try {
-    const jsonPath = path.join(process.cwd(), 'src', 'data', 'board.json');
-    const jsonData = await fs.readFile(jsonPath, 'utf-8');
-    const board: Board = JSON.parse(jsonData);
+    const board = await kv.get<Board>(BOARD_KEY);
+
+    if (!board) {
+      // KVにデータがない場合、jsonファイルから読み込んでKVにセットする
+      const initialBoard: Board = {
+        id: '1',
+        title: 'New Board',
+        lists: [],
+      }
+      return NextResponse.json(initialBoard);
+    }
     return NextResponse.json(board);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Error reading board data' }, { status: 500 });
+    return NextResponse.json({ message: 'Error reading board data from KV' }, { status: 500 });
   }
 }
 
@@ -27,15 +36,14 @@ export async function POST(request: Request) {
     const board = await request.json();
 
     // データ検証
-    boardSchema.parse(board)
-    const jsonPath = path.join(process.cwd(), 'src', 'data', 'board.json');
-    await fs.writeFile(jsonPath, JSON.stringify(board, null, 2));
-    return NextResponse.json({ message: 'Board data saved successfully' });
+    boardSchema.parse(board);
+    await kv.set(BOARD_KEY, board);
+    return NextResponse.json({ message: 'Board data saved successfully to KV' });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json({ message: 'JSONのバリデーションエラー', errors: error.errors }, { status: 400 })
     }
     console.error(error);
-    return NextResponse.json({ message: 'Error saving board data' }, { status: 500 });
+    return NextResponse.json({ message: 'Error saving board data to KV' }, { status: 500 });
   }
 }
